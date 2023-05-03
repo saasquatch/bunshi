2259e306920b04ed1db22bb0e5db30bb8b8cb9e5
himself65 <himself65@outlook.com>
2023-05-02 21:24:00 -0500

    Expose `createStore` API

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import { createScope } from "./scope";
 import { ScopeProvider } from "./ScopeProvider";
 import { useMolecule } from "./useMolecule";
 import { MoleculeScopeOptions } from "./useScopes";
+import { createStore } from './store';
+
 export {
   molecule,
+  createStore,
   createScope,
   ScopeProvider,
   useMolecule,
