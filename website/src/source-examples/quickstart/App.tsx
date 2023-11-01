61f17e35ffcfde7a9a6feee7fad0586dcf61f56e
Logan Volkers <loganvolkers@users.noreply.github.com>
2023-11-01 17:27:24 +0000

    Finish quickstart with live working examples

--- website/src/source-examples/quickstart/App.tsx
+++ website/src/source-examples/quickstart/App.tsx
@@ -3,24 +3,23 @@ import { useAtom } from "jotai";
 import React from "react";
 import { CountMolecule } from "./molecules";
 
+function Counter() {
+  const countAtom = useMolecule(CountMolecule);
+  const [count, setCount] = useAtom(countAtom);
+
+  return (
+    <div>
+      <p>Times clicked: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <Counter />
       <Counter />
-    </div>
-  );
-}
-
-function Counter() {
-  const countAtom = useMolecule(CountMolecule);
-
-  const [count, setCount] = useAtom(countAtom);
-
-  return (
-    <div>
-      <p>Times clicked: { count }</p>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
     </div>
   );
 }
---
+++ website/src/source-examples/quickstart/FormApp.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Counter from "./Counter.vue";
+import Form from "./Form.vue";
+</script>
+<template>
+  <div>
+    <Form name="one"
+      >
+      <Counter />
+      <Counter />
+    </Form>
+    <Form name="two"
+      >
+      <Counter />
+      <Counter />
+    </Form>
+  </div>
+</template>
