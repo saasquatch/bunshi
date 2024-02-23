a509948aef9735005101a51882e9eeb2bca5c4d5
andrewdoro <elfandreis@gmail.com>
2024-02-23 16:09:27 +0200

    Replace toSorted with array destructuring

--- src/vanilla/internal/scopeTupleSort.ts
+++ src/vanilla/internal/scopeTupleSort.ts
@@ -2,7 +2,7 @@ import type { AnyMoleculeScope, AnyScopeTuple } from "./internal-types";
 import { SortId } from "./symbols";
 
 export function scopeTupleSort(arr: AnyScopeTuple[]): AnyScopeTuple[] {
-  return arr.toSorted((a, b) => compareScopes(a[0], b[0]));
+  return [...arr].sort((a, b) => compareScopes(a[0], b[0]));
 }
 function compareScopes(a: AnyMoleculeScope, b: AnyMoleculeScope): number {
   return (a[SortId] as number) - (b[SortId] as number);
