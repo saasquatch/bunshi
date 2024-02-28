5c95d2b49850313d2fc05c4876dd0daa69036b84
andrewdoro <elfandreis@gmail.com>
2024-02-28 17:03:00 +0200

    fix: remove unused changes

--- src/vanilla/internal/scopeTupleSort.ts
+++ src/vanilla/internal/scopeTupleSort.ts
@@ -2,10 +2,8 @@ import type { AnyMoleculeScope, AnyScopeTuple } from "./internal-types";
 import { SortId } from "./symbols";
 
 export function scopeTupleSort(arr: AnyScopeTuple[]): AnyScopeTuple[] {
-  const t = "";
   return arr.toSorted((a, b) => compareScopes(a[0], b[0]));
 }
 function compareScopes(a: AnyMoleculeScope, b: AnyMoleculeScope): number {
   return (a[SortId] as number) - (b[SortId] as number);
 }
-fetch("stuff");
