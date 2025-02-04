32e095f13705e03356c18f064e44636cf83369e2
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2025-02-04 14:35:52 +0300

    docs(recipes): add Reatom recipe

---
+++ website/src/source-examples/reatom/Multiplier.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reatomRef, useAction } from "@reatom/npm-vue";
+import { useMolecule } from "bunshi/vue";
+import { MultiplierMolecule } from "./molecules";
+
+const { multiplierAtom } = useMolecule(MultiplierMolecule);
+const multiplier = reatomRef(multiplierAtom);
+const incrementMultiplier = useAction((ctx) =>
+  multiplierAtom(ctx, (multiplier) => multiplier + 1),
+);
+</script>
+
+<template>
+  <div>
+    <p>Multiplier: {{ multiplier }}</p>
+    <button @click="incrementMultiplier">increment</button>
+  </div>
+</template>
---
+++ website/src/source-examples/reatom/molecules2.ts
@@ -0,0 +1,28 @@
+import { ComponentScope, molecule } from "bunshi";
+import { atom, action } from "@reatom/core";
+
+export const MultiplierMolecule = molecule(() => ({
+  multiplierAtom: atom(0, "multiplier"),
+}));
+
+export const CountMolecule = molecule((mol, scope) => {
+  scope(ComponentScope);
+
+  const countAtom = atom(0, "countAtom");
+  const increment = action(
+    (ctx) => countAtom(ctx, (count) => count + 1),
+    "increment",
+  );
+
+  const { multiplierAtom } = mol(MultiplierMolecule);
+
+  const valueAtom = atom((ctx) => {
+    return ctx.spy(countAtom) * ctx.spy(multiplierAtom);
+  }, "valueAtom");
+
+  return {
+    countAtom,
+    valueAtom,
+    increment,
+  };
+});
